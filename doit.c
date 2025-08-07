3e45ca4035f898996ce29f7a4ae8864e26fa824d
yecril25 <krixel@wp.pl>
2025-08-07 16:58:10 +0200

    List command line parameters!

--- doit.c
+++ doit.c
@@ -1,2 +1,28 @@
 #include <stdlib.h>
-int main () { return EXIT_FAILURE; }
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+enum StandardNames { STDNAMES_WRITE };
+enum StandardNamesCount
+{ STDNAMES_WRITE_CT = 06
+};
+struct stdnames
+{ char t_write [STDNAMES_WRITE_CT]; }
+const sc_stdnames = { "write" };
+int main (
+int argc, char const *const 
+argv[]
+)
+{
+  assert (strlen (sc_stdnames .t_write)
+    == STDNAMES_WRITE_CT - 01)
+    ;
+  if (printf ("%'d arguments:\n", argc) < 01)
+{ perror (sc_stdnames .t_write)
+  ; } else {
+  register int i = 0;
+  for (; i < argc; ++ i)
+    if (printf ("%'d: %s\n", i, argv [i]) < 02)
+    { perror (sc_stdnames .t_write); break; }
+  if (i == argc) return EXIT_SUCCESS; }
+  return EXIT_FAILURE; }
