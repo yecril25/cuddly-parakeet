22b23a3c12d08e0f4e50a548373ab6edc9597a69
yecril25 <krixel@wp.pl>
2025-08-08 08:35:14 +0200

    Set locale

--- doit.c
+++ doit.c
@@ -5,11 +5,18 @@
 #include<assert.h>
 enum StandardNames { STDNAMES_WRITE };
 enum StandardNamesCount
-{ STDNAMES_WRITE_CT = 06
+{
+STDNAMES_WRITE_CT = 06,
+STDNAMES_LOCALE_CT = 07
 };
 struct stdnames
-{ char t_write [STDNAMES_WRITE_CT]; }
-const sc_stdnames = { "write" };
+{
+char
+t_write [STDNAMES_WRITE_CT],
+t_locale [STDNAMES_LOCALE_CT]
+; }
+const sc_stdnames = { "write", "locale"
+};
 int main (
 int argc, char const *const 
 argv[]
@@ -18,6 +25,29 @@ argv[]
   assert (strlen (sc_stdnames .t_write)
     == STDNAMES_WRITE_CT - 01)
     ;
+  assert
+    (strlen (sc_stdnames .t_locale)
+    == STDNAMES_LOCALE_CT - 01);
+  if
+    (setlocale
+    (LC_ALL,
+    sc_stdnames .t_locale
+    + STDNAMES_LOCALE_CT - 01))
+  {
+    register char const
+    *const my_message_locale
+    = setlocale (LC_MESSAGES, NULL);
+    if (my_message_locale)
+    { GET_MESSAGE_LOCALE_SUCCEEDED:
+      if (printf ("@%s\n", my_message_locale) < 01)
+      {
+        PRINT_LOCALE_FAILED:
+        perror (sc_stdnames .t_write); }
+      else { PRINT_LOCALE_SUCCEEDED: ; }}
+    else
+    {
+      GET_MESSAGE_LOCALE_FAILED:
+      perror (sc_stdnames .t_locale); }
   if (printf ("Arguments: %'d\n", argc) < 01)
 { perror (sc_stdnames .t_write)
   ; } else {
